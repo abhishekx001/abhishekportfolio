4c105aee20fb485d5283da43b339c989c3699d45
Abhishek S <abishek96335451@gmail.com>
2025-10-16 22:55:07 +0530

    no header

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Hero from "../components/Hero";
-import { NavBarDemo } from "../components/NavBarDemo.js";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <NavBarDemo />
       <Hero />
     </main>
   );
