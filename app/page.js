3a066316275368a8b642c7e1296d9db6f626b482
Abhishek S <abishek96335451@gmail.com>
2025-10-16 22:47:05 +0530

    Remove header completely from website

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Hero from "../components/Hero";
-import { Header } from "../components/Header.js";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Header />
       <Hero />
     </main>
   );
