9775fc847a159c0692ab30c972094d4aad8f738f
Abhishek S <abishek96335451@gmail.com>
2025-10-20 16:34:43 +0530

    one last commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: 'loose'
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
